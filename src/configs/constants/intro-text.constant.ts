2f0871d0eb6d71fb930f0a8f34bbd1e0a2acd978
gerpan <nhaths4701@gmail.com>
2022-03-08 10:36:37 +0700

    Change text intro

--- src/configs/constants/intro-text.constant.ts
+++ src/configs/constants/intro-text.constant.ts
@@ -1,7 +1,7 @@
 export const INTRO_TEXT = {
   pText1: 'Hello 👋!!!',
   pText2:
-    'My name is Nhat and I am a Full Stack Developer and an open source lover from Vietnam 🇻🇳.',
+    'My name is Nhat and I am a Developer and an open source lover from Vietnam 🇻🇳.',
   pText3:
     "This is my portfolio, where I write about the things I'm working on and share my experience.",
 };
