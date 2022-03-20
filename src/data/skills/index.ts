1e9516aa6bd72bed6f1ab514dd862f22e861ea87
gerpan <nhaths4701@gmail.com>
2022-03-20 21:33:46 +0700

    Fix dupplicate code

--- src/data/skills/index.ts
+++ src/data/skills/index.ts
@@ -1,2 +1,13 @@
-export { WEB_DEV_SKILLS } from './web-dev';
-export { TOOL_SKILLS } from './tool';
+import { WEB_DEV_SKILLS } from './web-dev';
+import { TOOL_SKILLS } from './tool';
+
+export const SKILLS_DATA = [
+  {
+    title: 'Web Development',
+    data: WEB_DEV_SKILLS,
+  },
+  {
+    title: 'Tools',
+    data: TOOL_SKILLS,
+  },
+];
--- src/pages/skills/index.tsx
+++ src/pages/skills/index.tsx
@@ -4,7 +4,7 @@ import Layout from '@/templates/layout';
 import PageHeading from '@/components/commons/page-heading';
 import { MotionDiv } from '@/components/animations';
 import { curveTransition } from '@/components/animations/transitions';
-import { TOOL_SKILLS, WEB_DEV_SKILLS } from '@/data/skills';
+import { SKILLS_DATA } from '@/data/skills';
 import SkillCard from '@/components/skills/skill-card';
 import SubHeading from '@/components/commons/page-heading/sub-heading';
 
@@ -13,59 +13,34 @@ const SkillsPage: NextPage = () => {
     <Layout title="Skills | Gerpan" description="Gerpan personal website">
       <PageHeading>My skills</PageHeading>
 
-      <div className="mt-8">
-        <SubHeading>Web Development</SubHeading>
-        <MotionDiv
-          transition={{
-            ...curveTransition,
-            delayChildren: 0.5,
-            staggerChildren: 0.2,
-          }}
-          initial="hidden"
-          animate="show"
-          className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-x-4 gap-y-6"
-        >
-          {WEB_DEV_SKILLS.map((item, key) => (
-            <MotionDiv
-              key={key}
-              variants={{
-                hidden: { opacity: 0, x: -50 },
-                show: { opacity: 1, x: 0 },
-              }}
-              whileHover={{ translateY: '-5%' }}
-            >
-              <SkillCard {...item} />
-            </MotionDiv>
-          ))}
-        </MotionDiv>
-      </div>
-
-      <div className="mt-8">
-        <SubHeading>Tools</SubHeading>
-        <MotionDiv
-          transition={{
-            ...curveTransition,
-            delayChildren: 0.5,
-            staggerChildren: 0.2,
-          }}
-          initial="hidden"
-          animate="show"
-          className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-x-4 gap-y-6"
-        >
-          {TOOL_SKILLS.map((item, key) => (
-            <MotionDiv
-              key={key}
-              variants={{
-                hidden: { opacity: 0, x: -50 },
-                show: { opacity: 1, x: 0 },
-              }}
-              whileHover={{ translateY: '-5%' }}
-            >
-              <SkillCard {...item} />
-            </MotionDiv>
-          ))}
-        </MotionDiv>
-      </div>
+      {SKILLS_DATA.map((skill, index) => (
+        <div key={index} className="mt-8">
+          <SubHeading>{skill.title}</SubHeading>
+          <MotionDiv
+            transition={{
+              ...curveTransition,
+              delayChildren: 0.5,
+              staggerChildren: 0.2,
+            }}
+            initial="hidden"
+            animate="show"
+            className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-x-4 gap-y-6"
+          >
+            {skill.data.map((item, key) => (
+              <MotionDiv
+                key={key}
+                variants={{
+                  hidden: { opacity: 0, x: -50 },
+                  show: { opacity: 1, x: 0 },
+                }}
+                whileHover={{ translateY: '-5%' }}
+              >
+                <SkillCard {...item} />
+              </MotionDiv>
+            ))}
+          </MotionDiv>
+        </div>
+      ))}
     </Layout>
   );
 };
