1c848fcb4fe51fd540d3af1d2b2fe7ceb9f37fbe
gerpan <nhaths4701@gmail.com>
2022-03-21 09:59:04 +0700

    Update career

--- src/data/about/career.ts
+++ src/data/about/career.ts
@@ -2,22 +2,8 @@ export const CAREER_DATA = [
   {
     title: 'Devzone',
     subtitle: 'Software Developer',
-    time: '2020 - Present',
+    time: '2020 - 2021',
     detail:
-      'Develop Web/App for outsource products. The main technology are ReactJS, NodeJS, React Native, AWS,...',
-  },
-  {
-    title: 'Devzone enozved',
-    subtitle: 'Software Architect',
-    time: '2020 - Present',
-    detail:
-      'Develop Web/App for outsource products. The main technology are ReactJS, NodeJS, React Native, AWS,...',
-  },
-  {
-    title: 'Enozved devzone',
-    subtitle: 'Project Management',
-    time: '2020 - Present',
-    detail:
-      'Develop Web/App for outsource products. The main technology are ReactJS, NodeJS, React Native, AWS,...',
+      'Develop Web/App outsource products using Microservices architecture, NodeJS, ReactJS, NextJS, React Native, SQL, AWS,...',
   },
 ];
